eb39dfcbc5302f14023fab404cf709ac0e59a9b7
20pa1a5708 <113027359+20pa1a5708@users.noreply.github.com>
2023-08-03 01:07:23 +0530

    uniqueCharactersCount.java

---
+++ uniqueCharactersCount.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+    String str=input.next();
+    HashSet<Character> set=new HashSet<>();
+    for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        set.add(ch);
+    }
+    System.out.println(set.size());
+    }
+}
